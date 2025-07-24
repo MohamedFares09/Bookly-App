aae7974a9a88f75442c20c717c7a9146a0d723e9
MohamedFares09 <mofares010@gmail.com>
2025-07-25 02:07:49 +0300

    created splah screen

---
+++ lib/features/splash/presintation/views/splah_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/features/splash/presintation/views/widgets/splah_view_body.dart';
+import 'package:flutter/material.dart';
+
+class SplahView extends StatelessWidget {
+  const SplahView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SplahViewBody(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,20 @@
+import 'package:bookly_app/features/splash/presintation/views/splah_view.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: SplahView(),
     );
   }
 }
